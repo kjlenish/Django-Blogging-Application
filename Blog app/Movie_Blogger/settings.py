fa283b62cb2316a920c59ac18b4e6cce3427a0ac
Lenish K J <lenish.kj@flytxt.com>
2024-08-10 21:56:09 +0530

    updated settings.py

--- Blog app/Movie_Blogger/settings.py
+++ Blog app/Movie_Blogger/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lenishkj@gmail.com'
-EMAIL_HOST_PASSWORD = 'gzlbcbyoktxowuew'
+EMAIL_HOST_USER = 'lkj@gmail.com'
+EMAIL_HOST_PASSWORD = 'password123'
